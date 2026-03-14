cf17a165aa84ea0577ef7847fb58fff7d07b9e07
matt <matelhov@gmail.com>
2026-03-14 11:27:47 +0300

    создали врагов

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from random import randint
 import pygame
-
 
 class Sprite:
     def __init__(self, center, image):
@@ -36,6 +36,8 @@ image = pygame.Surface((50,50))
 image.fill("green ")
 player = Sprite(center, image)
 bullets = []
+enemies = []
+score = 0
 
 runing = True
 while runing:
@@ -51,17 +53,61 @@ while runing:
             bullet = MoveSprite( center, image, 7, direction )
             bullets.append(bullet)
 
+
+    if randint(0,100) <= 5:
+        image = pygame.Surface((20,20))
+        image.fill("#43bccc")
+        center = pygame.Vector2(player.rect.center)
+
+        r = randint(1, 4)
+        if r == 1:
+            pos = pygame.Vector2(randint(0 , window_size[0]),-100)
+        if r == 2:
+            pos = pygame.Vector2(window_size[0] +100, randint(0,window_size[1]))
+        elif r == 3:
+            pos = pygame.Vector2(randint(0 , window_size[0]), window_size[1]+100)
+        else:
+            pos = pygame.Vector2(-100,randint(0 , window_size[1]))
+
+        direction = center - pos
+        enemy = MoveSprite(pos, image, randint(100,400) / 100, direction)
+        enemies.append(enemy)
+
     for bullet in bullets:
         bullet.updata()
 
-    surface.fill("red")
+    for enimy in enemies:
+        enimy.updata()
+
+    surface.fill("blue")
     player.render(surface)
+
     for bullet in bullets:
         bullet.render(surface)
+    for enimy in enemies:
+        enimy.render(surface)
+    for bullet in bullets:
+        for enemy in enemies:
+            if bullet.rect.colliderect(enemy.rect):
+                score += 1
+                bullets.remove(bullet)
+                enemies.remove(enemy)
+                break
+
+
+    for enemy in enemies:
+        if player.rect.colliderect(enemy.rect):
+            score = 0
+            enemies.clear()
+            bullets.clear()
+            break
+
     window.flip()
 
     clock.tick(max_fps)
     print(bullets)
+
+
 
 
 
